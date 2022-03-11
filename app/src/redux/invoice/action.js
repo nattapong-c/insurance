d5a94c2843a826d8c6396e6616588dccfdf7f289
nattapong-c <bozshijack@gmail.com>
2022-03-11 20:22:11 +0700

    fix type action delete invoice

--- app/src/redux/invoice/action.js
+++ app/src/redux/invoice/action.js
@@ -34,15 +34,15 @@ export const getInvoiceListAction = (params) => async (dispatch) => {
 
 export const deleteInvoiceAction = (data) => async (dispatch) => {
     if (data?.clear) {
-        dispatch({ type: TYPE.INVOICE_UPDATE_CLEAR });
+        dispatch({ type: TYPE.INVOICE_DELETE_CLEAR });
         return;
     }
-    dispatch({ type: TYPE.INVOICE_UPDATE_REQ });
+    dispatch({ type: TYPE.INVOICE_DELETE_REQ });
     try {
         const response = await API.deleteInvoiceList(data);
-        dispatch({ type: TYPE.INVOICE_UPDATE_SUCCESS, payload: response.data });
+        dispatch({ type: TYPE.INVOICE_DELETE_SUCCESS, payload: response.data });
     } catch (err) {
-        dispatch({ type: TYPE.INVOICE_UPDATE_FAIL, payload: err.response.data.error });
+        dispatch({ type: TYPE.INVOICE_DELETE_FAIL, payload: err.response.data.error });
     }
 };
 
