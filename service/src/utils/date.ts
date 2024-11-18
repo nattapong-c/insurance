bd696086855a647b795bff364cdf41fc1ed4cf91
Nattapong <nattapong@bizcuitsolution.com>
2024-11-18 20:20:02 +0700

    test timezone

--- service/src/utils/date.ts
+++ service/src/utils/date.ts
@@ -1,3 +1,10 @@
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export const convertToThaiDate = (dateObj: Date): string => {
-    return new Intl.DateTimeFormat("th-TH", { month: "long", day: "numeric", year: "numeric" }).format(dateObj);
+    return new Intl.DateTimeFormat("th-TH", { month: "long", day: "numeric", year: "numeric", timeZone: "Asia/Bangkok" }).format(dateObj);
 }
