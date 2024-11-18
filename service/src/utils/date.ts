722096ebb34bfdf9521806da725c03f9805b8e5d
Nattapong <nattapong@bizcuitsolution.com>
2024-11-18 20:22:52 +0700

    remove dayjs

--- service/src/utils/date.ts
+++ service/src/utils/date.ts
@@ -1,10 +1,3 @@
-import dayjs from "dayjs";
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
 export const convertToThaiDate = (dateObj: Date): string => {
     return new Intl.DateTimeFormat("th-TH", { month: "long", day: "numeric", year: "numeric", timeZone: "Asia/Bangkok" }).format(dateObj);
 }
